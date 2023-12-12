4e241a1e21e439e7769469d4bb53b15451bfcae0
Hakan Durmaz <hakan.durmaz@atmosware.turkcell.com.tr>
2023-12-12 08:17:17 +0300

    added answer of part2 of day2

--- src/main/java/guru/hakandurmaz/day2/part2/CubeConundrumPart2.java
+++ src/main/java/guru/hakandurmaz/day2/part2/CubeConundrumPart2.java
@@ -1,24 +1,23 @@
-package guru.hakandurmaz.day2.part1;
+package guru.hakandurmaz.day2.part2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Pattern;
 
-public class CubeConundrum {
+public class CubeConundrumPart2 {
 
-    public static final String FILE_PATH = "D:\\projects\\adventofcode-2023\\src\\main\\java\\guru\\hakandurmaz\\day2\\part1\\input.txt";
+    public static final String FILE_PATH = "D:\\projects\\adventofcode-2023\\src\\main\\java\\guru\\hakandurmaz\\day2\\part2\\input.txt";
     static HashMap<String, Integer> hashMap = new HashMap<>();
 
     public static void main(String[] args) {
-        int result = calculateSumOfGameIds();
+        int result = calculateSumOfCubes();
         System.out.println("Result: " + result);
     }
 
-    private static int calculateSumOfGameIds() {
-        int count = 0;
+    private static int calculateSumOfCubes() {
+        int sum = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
             var pattern = Pattern.compile("\\b\\d+\\s+\\w+\\b");
@@ -35,32 +34,15 @@ public class CubeConundrum {
                         hashMap.put(color, hashMap.get(color) > Integer.parseInt(number) ? hashMap.get(color) : Integer.parseInt(number));
                     }
                 }
-                boolean checked = checkIfGameIsPossible(hashMap);
+                Integer sumOfLine = hashMap.values()
+                    .stream()
+                    .reduce(1, (a, b) -> a * b);
+                sum += sumOfLine;
                 hashMap.clear();
-                if (checked) {
-                    int i = extractGameNumber(line);
-                    count += i;
-                }
             }
-            return count;
+            return sum;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static boolean checkIfGameIsPossible(Map<String, Integer> hashMap) {
-        var red = hashMap.get("red");
-        var green = hashMap.get("green");
-        var blue = hashMap.get("blue");
-        return red <= 12 && green <= 13 && blue <= 14;
-    }
-
-    private static int extractGameNumber(String input) {
-        var pattern = Pattern.compile("Game (\\d+):");
-        var matcher = pattern.matcher(input);
-        if (matcher.find()) {
-            return Integer.parseInt(matcher.group(1));
-        }
-        return -1;
     }
 }
